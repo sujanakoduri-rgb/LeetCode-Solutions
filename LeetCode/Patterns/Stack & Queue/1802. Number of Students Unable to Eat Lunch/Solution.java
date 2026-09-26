705d43047953943acc0bd1a9f59232d22982a361
sujanakoduri-rgb <sujanakoduri@gmail.com>
2026-09-26 19:17:59 +0530

    Add Easy Java Solution for 1802. Number of Students Unable to Eat Lunch from LeetCode

---
+++ LeetCode/Patterns/Stack & Queue/1802. Number of Students Unable to Eat Lunch/Solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < students.length; i++) {
+            q.add(students[i]);
+        }
+        Stack<Integer> st = new Stack<>();
+        for(int i = sandwiches.length - 1; i >= 0; i--) {
+            st.push(sandwiches[i]);
+        }
+        int count = 0;
+        while(!q.isEmpty() && count < q.size()) {
+            if(q.peek() == st.peek()) {
+                q.poll();
+                st.pop();
+                count = 0;
+            }
+            else {
+                q.add(q.poll());
+                count++;
+            }
+        }
+        return q.size();
+    }
+}
